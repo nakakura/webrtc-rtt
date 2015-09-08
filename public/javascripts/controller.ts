29ddf24a034e000f6f85385f1fe78b59bf5ac364
Toshiya Nakakura <t.nakakura@ntt.com>
2015-09-08 16:30:37 +0900

    update apple v6

--- public/javascripts/controller.ts
+++ public/javascripts/controller.ts
@@ -2,13 +2,25 @@
 /// <reference path="./models/peer_manager.ts" />
 
 window.onload = function(){
-    var peer = new RobotServer.PeerManager('robot', {
+    var peer = new RobotServer.PeerManager("oculus-video", {
         config: {"iceServers":[
-            {"url": "stun:stun.skyway.io:3478"}
-        ],optional: [{RtpDataChannels: true}]},
+            {"url": "stun:stun.skyway.io:3478"},
+            {
+                "url": 'turn:biztest-turn.skyway.io:443?transport=udp',
+                'credential': 'wI+8tZRjUllYctlWD/sAvM9yutM=',
+                'username': 'rinrin_1234'
+            },
+            {
+                "url": 'turn:biztest-turn.skyway.io:443?transport=tcp',
+                'credential': 'wI+8tZRjUllYctlWD/sAvM9yutM=',
+                'username': 'rinrin_1234'
+            }
+        ]},
         port: 443,
         secure: true,
-        debug: 3} );
+        key: '62ac6ff0-6863-455a-a214-83a1cb6c9a82',
+        debug: 3});
+
     peer.addNeighbor(new RobotServer.Neighbor("oculus-video", RobotServer.NeighborTypeEnum.video));
     peer.addNeighbor(new RobotServer.Neighbor("oculus-data", RobotServer.NeighborTypeEnum.data));
 
